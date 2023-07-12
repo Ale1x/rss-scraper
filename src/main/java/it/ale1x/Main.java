fb58ee96226b1ad55cfa1ac1794a48bfd397b09b
Alessandro Passarelli <me@passarelli.dev>
2023-07-12 18:34:06 +0200

    Add initial project files and setup
    This commit includes all the initial project setup, including the creation of necessary .gitignore files, pom.xml, vcs.xml, encodings.xml, and misc.xml. Some Java source files (RssUrl.java, Main.java) were also created that establish scraping operations from an RSS feed. The .gitignore file was configured to ignore unnecessary system, IDE, and build files, keeping the repository clean. This initial setup is fundamental to get the project up and running.

--- src/main/java/it/ale1x/Main.java
+++ src/main/java/it/ale1x/Main.java
@@ -1,26 +1,26 @@
 package it.ale1x;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
-import org.w3c.dom.Element;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 public class Main {
     public static void main(String[] args) {
-
         try {
             String url = "https://www.ilsole24ore.com/rss/mondo--europa.xml";
             URLConnection connection = new URL(url).openConnection();
-
             connection.connect();
 
             BufferedReader response = new BufferedReader(new InputStreamReader(connection.getInputStream(),
@@ -29,34 +29,24 @@ public class Main {
             StringBuilder sb = new StringBuilder();
             String line;
 
-            File inputFile = new File("input.txt");
-            FileWriter fw = null;
-            try {
-                fw = new FileWriter(inputFile);
-            }catch(Exception e) {
-                e.printStackTrace();
-            }
-
-            while((line = response.readLine()) != null) {
+            while ((line = response.readLine()) != null) {
                 sb.append(line);
-                sb.append(System.lineSeparator());
-                fw.write(line);
-                fw.write("\n");
             }
 
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(inputFile);
+            Document doc = dBuilder.parse(new InputSource(new StringReader(sb.toString())));
             doc.getDocumentElement().normalize();
-            System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
-            NodeList nList = doc.getElementsByTagName("title");
-            System.out.println("----------------------------");
 
+            System.out.println("Root element: " + doc.getDocumentElement().getNodeName());
+            NodeList nList = doc.getElementsByTagName("title");
+
+            System.out.println("----------------------------");
             for (int temp = 0; temp < nList.getLength(); temp++) {
                 Node nNode = nList.item(temp);
-                System.out.println("\nCurrent Element :" + nNode.getNodeName());
+                System.out.println("\nCurrent Element: " + nNode.getNodeName());
             }
-        } catch (Exception e) {
+        } catch (IOException | ParserConfigurationException | SAXException e) {
             e.printStackTrace();
         }
     }
